132b34c92ca64647910cc2208da5f27877950e5a
Arjun Wadhwa <arjun.wadhwa@walmartlabs.com>
2019-03-24 00:42:40 +0530

    update contact

--- src/Containers/ContactPage/index.js
+++ src/Containers/ContactPage/index.js
@@ -1,22 +1,69 @@
 import React from 'react';
 import Text from '../../Components/Text';
 import Image from '../../Components/Image';
-import Button from '../../Components/Button';
-import Input from '../../Components/Input';
 import Footer from '../../Components/Footer';
 import WOW from "wowjs";
+
+import axios from 'axios';
 
 class Contact extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            name:"",
+            email:"",
+            phone:"",
+            company:"",
+            message:""
+        }
     }
 
     componentDidMount() {
         const wow = new WOW.WOW();
         wow.init();
         window.scrollTo(0,0);
+    }
+
+    sendMail(){
+        console.log(this.refs.name)
+        var body = {
+            name: this.state.name,
+            email: this.state.email,
+            phone: this.state.phone,
+            company: this.state.company,
+            message: this.state.message
+        }
+        if(!body.name || !body.email || !body.phone || !body.company || !body.message){
+            alert("Please enter all the fields.")
+        }
+        else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(body.email)){
+            if((body.phone.match(/^\d{10}$/))){
+                axios.post("https://yura-mail-service.herokuapp.com/mail",body).then(res => {
+                    if(res.status==200){
+                        alert("your message has been delivered to Dennis!")
+                        this.setState({
+                            name:"",
+                            email:"",
+                            phone:"",
+                            company:"",
+                            message:""
+                        })
+                    }
+                })
+                .catch(err =>{
+                    alert(err)
+                }) 
+            }
+            else{
+                alert("Please enter a valid contact number.")
+            }
+            
+        }
+        else{
+             alert("Please enter a valid email address.")
+        }
+        
     }
 
     render() { 
@@ -36,19 +83,19 @@ class Contact extends React.Component {
                             <Text body="Fill this out so Dennis can learn about you and your needs." className="contactHeadRight"/>
                             <div className="form">
                                 <Text body="Hi, my name is &nbsp;" className="formText inline"/>
-                                <Input type="text" placeholder="Full name" className="formInput inline"/>
+                                <input type="text" placeholder="Full name" className="formInput inline" value={this.state.name} onChange={(name)=> this.setState({name: name.target.value})}/>
                                 <Text body="&nbsp;." className="formText inline"/>
                                 <Text body="I work for&nbsp;" className="formText inline"/><br/>
-                                <Input type="text" placeholder="Company name" className="formInput inline"/>
+                                <input type="text" placeholder="Company name" className="formInput inline"  value={this.state.company} onChange={(company)=> this.setState({company: company.target.value})}/>
                                 <Text body="&nbsp;and I am in need of a partner" className="formText inline"/><br/>
                                 <Text body="to assist me with&nbsp;" className="formText inline"/>
-                                <Input type="text" placeholder="in a nutshell" className="formInput inline"/><br/>
+                                <input type="text" placeholder="in a nutshell" className="formInput inline"  value={this.state.message} onChange={(message)=> this.setState({message: message.target.value})}/><br/>
                                 <Text body="You can reach me at&nbsp;" className="formText inline"/>
-                                <Input type="text" placeholder="email address" className="formInput"/><br/>
+                                <input type="text" placeholder="email address" className="formInput" value={this.state.email} onChange={(email)=> this.setState({email: email.target.value})}/><br/>
                                 <Text body="or&nbsp;" className="formText inline"/>
-                                <Input type="text" placeholder="telephone number" className="formInput"/>
+                                <input type="text" placeholder="telephone number" className="formInput" value={this.state.phone} onChange={(phone)=> this.setState({phone: phone.target.value})}/>
                                 <Text body="to get the conversation started. Thanks!" className="formText"/>
-                                <Button link="" text="Send Dennis a message" className="contactButton"/>
+                                <button className="contactButton"  onClick={()=>this.sendMail()}>Send Dennis a message &nbsp;<i className="fa fa-angle-right" aria-hidden="true" ></i></button>
                             </div>
                         </div>
                     </div>
